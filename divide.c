7f5ae8e797b2c25011fc3f99748e4f4f32ccb887
pooja2802 <pooja1201a@gmail.com>
2015-10-12 16:40:24 +0530

    division of 2 numbers

---
+++ divide.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter a: ");
+scanf("%d",&a);
+printf("Enter b: ");
+scanf("%d",&b);
+c=a/b;
+printf("Division=%d",c)
+}
